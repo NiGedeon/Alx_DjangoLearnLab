e9df6f1c42a6e43b56d7888aa9c24f7b83a692ee
NiGedeon <nigideon300@gmail.com>
2024-11-10 10:35:04 -0800

    Update made

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -12,6 +12,6 @@ def get_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
+    library = Library.objects.get(library=library_name)
     return library.librarian
 
