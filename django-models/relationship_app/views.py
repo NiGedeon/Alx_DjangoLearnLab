0949b9fbeb1be52a11457e93b5c1c469300a6f3c
NiGedeon <nigideon300@gmail.com>
2024-11-10 13:04:27 -0800

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -48,16 +48,16 @@ class RegisterView(View):
 
 
 # Admin View
-@user_passes_test(lambda user: check_role(user, 'Admin'))
+@user_passes_test(lambda user: user.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 # Librarian View
-@user_passes_test(lambda user: check_role(user, 'Librarian'))
+@user_passes_test(lambda user: user.userprofile.role == 'Librarian'))
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 # Member View
-@user_passes_test(lambda user: check_role(user, 'Member'))
+@user_passes_test(lambda user: user.userprofile.role == 'Member'))
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
