7106358fecc19ae6164206ab0a8070262397ca9b
NiGedeon <nigideon300@gmail.com>
2024-11-23 13:47:40 -0800

    update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics,viewsets
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+import rest_framework.permissions.IsAuthenticated
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
