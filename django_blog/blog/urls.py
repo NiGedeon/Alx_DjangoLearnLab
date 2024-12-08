986088746f7cbce54a0d1b380f12867f54f76a6e
NiGedeon <nigideon300@gmail.com>
2024-12-08 08:46:53 -0800

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,3 +23,10 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
+
+urlpatterns = [
+    path('posts/<int:pk>/', views.CommentDetailView.as_view(), name='post-detail'),
+    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('posts/<int:post_id>/comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-edit'),
+    path('posts/<int:post_id>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+]
