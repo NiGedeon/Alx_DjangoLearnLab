772f0e1d9856f8cac99360b6fdd7a73673487785
NiGedeon <nigideon300@gmail.com>
2024-12-08 13:20:55 -0800

    updated

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -23,14 +23,16 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
 
-
-from django.forms import widgets  # This is the correct import for widgets
+# blog/forms.py
+from django import forms
+from taggit.forms import TagWidget  # Import TagWidget from django-taggit
+from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Example fields
+        fields = ['title', 'content', 'tags']  # Ensure tags is included
         widgets = {
-            'tags': widgets.CheckboxSelectMultiple(),  # Example widget for tag field
+            'tags': TagWidget()  # Use TagWidget to render the tags field
         }
 
