18053e379520cf21d04d0f4d86fd794725e8db8c
NiGedeon <nigideon300@gmail.com>
2024-12-08 08:06:57 -0800

    update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -42,3 +42,54 @@ def profile(request):
         form = UserChangeForm(instance=request.user)
     return render(request, 'profile.html', {'form': form})
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]  # Show newest posts first
+    paginate_by = 5  # Add pagination, showing 5 posts per page
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "post_form.html"
+
+    def form_valid(self, form):
+        # Automatically assign the logged-in user as the author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        # Only allow the author to update their post
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        # Only allow the author to delete their post
+        post = self.get_object()
+        return self.request.user == post.author
+
