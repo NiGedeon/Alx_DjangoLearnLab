1df75825b5347208a785ca8a16f512dd958d78db
NiGedeon <nigideon300@gmail.com>
2024-12-08 08:33:46 -0800

    update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -16,3 +16,9 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content']
 
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -93,3 +93,57 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return self.request.user == post.author
 
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Comment
+from .forms import CommentForm
+
+# To view a blog post along with its comments
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()  # Get all comments related to the post
+    comment_form = CommentForm()
+
+    if request.method == "POST" and request.user.is_authenticated:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.author = request.user
+            new_comment.save()
+            return HttpResponseRedirect(post.get_absolute_url())
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form
+    })
+
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DeleteView
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'comment_form.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'comment_confirm_delete.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk}
+
+
