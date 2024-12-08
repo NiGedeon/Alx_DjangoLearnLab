2aa3a1112ba92be77d825cc47ac78da3759385a7
NiGedeon <nigideon300@gmail.com>
2024-12-08 06:55:14 -0800

    update

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,4 +5,12 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete = models.CASCADE,related_name = "author")
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+#Creating view by Calling and using the custom User creation form to handle the creation form
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+#The view to handle The user login
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+#The view to handle user logout
+from django.contrib.auth.views import LogoutView
+
+class CustomLogoutView(LogoutView):
+    next_page = '/'
+
+#The view to handle viewing and editing profile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile.html', {'form': form})
+
