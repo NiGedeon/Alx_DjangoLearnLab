9b9e973344735520ee71b4df226a73feb70e7cc5
NiGedeon <nigideon300@gmail.com>
2024-12-08 09:00:48 -0800

    Updates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete = models.CASCADE,related_name = "author")
+    tags = TaggableManager()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
