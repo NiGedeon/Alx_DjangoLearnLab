2573ad296dbbfdf017410d2f8760f8980c988ae5
NiGedeon <nigideon300@gmail.com>
2024-12-14 12:32:34 -0800

    update

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,generics
-from rest_framework.views import APIView
+from django.shortcuts import render
+from rest_framework.views import APIView,generics
+from rest_framework import permissions.IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
