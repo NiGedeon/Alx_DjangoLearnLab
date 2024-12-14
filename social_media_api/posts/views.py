1ac5be28463d5906ebdf60e6f1803b673dd5085f
NiGedeon <nigideon300@gmail.com>
2024-12-14 11:06:42 -0800

    update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from rest_framework import filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -6,6 +7,9 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+     filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
